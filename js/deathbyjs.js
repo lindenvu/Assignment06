33b996efaa77e5911a7af1c47dbaf72f3df27f74
lindenvu <github@lindenvu.com>
2018-04-05 21:07:10 -0700

    Update 3

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,7 +1,11 @@
 /*eslint-env browser*/
 
 //STEP 1
-
+function alphabetize($str1) {
+    "use strict";
+    return $str1.toLowerCase().split("").sort().join("");
+}
+window.alert(alphabetize(window.prompt("Enter a word")));
 //STEP 2
 
 //STEP 3
