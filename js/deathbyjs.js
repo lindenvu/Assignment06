bcb25e38c1a1bf329ec4d7a3d37e8447584f4b07
lindenvu <github@lindenvu.com>
2018-04-05 21:37:26 -0700

    Update 4

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,13 +1,17 @@
 /*eslint-env browser*/
 
 //STEP 1
-function alphabetize($str1) {
+function alphabetize(str1) {
     "use strict";
-    return $str1.toLowerCase().split("").sort().join("");
+    return str1.toLowerCase().split("").sort().join("");
 }
 window.alert(alphabetize(window.prompt("Enter a word")));
 //STEP 2
-
+function capitalize(str2) {
+    "use strict";
+    return str2.replace(/(\b\w)/gi,function(m){return m.toUpperCase();});
+}
+window.alert(capitalize(window.prompt("Enter a sentence")));
 //STEP 3
 
 //STEP 4
