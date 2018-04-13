41b93e15267c681f322f08055b506036b1b605d9
Linden Vu <lvu@dexcom.com>
2018-04-12 17:56:54 -0700

    update 1

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -9,11 +9,25 @@ window.alert(alphabetize(window.prompt("Enter a word")));
 //STEP 2
 function capitalize(str2) {
     "use strict";
-    return str2.replace(/(\b\w)/gi,function(m){return m.toUpperCase();});
+    return str2.replace(/(\b\w)/gi, function (m) {return m.toUpperCase(); });
 }
 window.alert(capitalize(window.prompt("Enter a sentence")));
-//STEP 3
+//STEP 3 
+function countVowels(str3) {
+    "use strict";
+    return str3.match("a/g");
+}
+window.alert(countVowels(window.prompt("Enter a sentence")));
+//STEP 4 
+function randomString(num4) {
+    "use strict";
+    var x, y = "", z = parseInt(num4, 10);
+    for (x = 0; x < z; x += 1) {
+        y += String.fromCharCode(Math.floor(Math.random() * 127));
+    }
+    return y;
+}
+window.alert(randomString(window.prompt("Enter a number")));
 
-//STEP 4
 
 //STEP 5
